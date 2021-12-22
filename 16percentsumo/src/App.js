a1c27b74fa0cb393853bcf5425e87526a0ea92c0
Haal20 <alma-harlin@hotmail.com>
2021-12-22 10:11:55 +0100

    init commit

---
+++ 16percentsumo/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          16% SUMO
+        </h1>
+        Listen at: <a
+          className="App-link"
+          href="https://soundcloud.com/user-504018828"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Soundcloud
+        </a>
+        Checkout: <a
+          className="App-link"
+          href="https://www.instagram.com/16_percent_sumo/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instagram
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
