e64b466faaf1241f25109bead6cdbd02e5a32e3f
Haal20 <alma-harlin@hotmail.com>
2022-01-20 17:54:50 +0100

    ver next level

--- 16percentsumo/src/App.js
+++ 16percentsumo/src/App.js
@@ -1,28 +1,99 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          16% SUMO
-        </h1>
-        Listen at: <a
+        <div>
+            <img src="../watermark.png" alt="logo" />
+          <h1>
+            16% SUMO
+          </h1>
+          Music Producer & Content Creator
+          <div>
+          <a
+            id="insta"
+            className="App-link"
+            href="https://www.instagram.com/16_percent_sumo/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+          id="mail"
+            className="App-link"
+            href="mailto:16percentsumo@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            mail
+          </a>
+          </div>
+        </div>
+        <br/>
+        <div className="App-btn">
+        <a
           className="App-link"
-          href="https://soundcloud.com/user-504018828"
+          href="https://distrokid.com/hyperfollow/16sumo/sprinkles"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Soundcloud
+          Pre-save Sprinkles by 16% SUMO
         </a>
-        Checkout: <a
+        </div>
+        <div className="App-btn">
+        <a
           className="App-link"
-          href="https://www.instagram.com/16_percent_sumo/"
+          href="https://open.spotify.com/playlist/22C4LM3J0Xmi8aqXJ7igDH?si=3fc8bd45e52c4d14"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Instagram
+         Playlist: Vibes (Lofi/Chill/Relax)
         </a>
+        </div>
+        <div className="App-btn">
+        <a
+          className="App-link"
+          href="https://soundcloud.com/user-504018828?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          SoundCloud
+        </a>
+        </div>
+        <div className="App-btn">
+        <a
+          className="App-link"
+          href="https://open.spotify.com/artist/11RM3Vnm6zN9mpEJkdnDiO?si=oDZcIpKoQYOALEL1bZG_GQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Spotify
+        </a>
+        </div>
+        <div className="App-btn">
+        <a
+          className="App-link"
+          href="https://www.facebook.com/16percentsumo/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Facebook
+        </a>
+        </div>
+        <div className="App-btn">
+        <a
+          className="App-link"
+          href="https://www.youtube.com/channel/UC0f1nulbOh9rpb37XTkOKVg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Beat videos & live cookups YouTube channel
+        </a>
+        </div>
+        <iframe src="https://open.spotify.com/embed/playlist/22C4LM3J0Xmi8aqXJ7igDH?utm_source=generator" width="33%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
       </header>
     </div>
   );
